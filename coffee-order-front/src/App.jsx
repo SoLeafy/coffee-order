0606da868d8664d6107d8ded6dd3498c2d9ce46a
Jinseon Lee <leaftree@kakao.com>
2025-05-06 00:34:28 +0900

    setting: spring boot + react

---
+++ coffee-order-front/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Coffee Order</h1>
+      <p>By Bear & Rabbit</p>
+    </>
+  );
+}
+
+export default App;
---
+++ coffeeorder/src/test/java/com/bearnrabbit/coffeeorder/CoffeeorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bearnrabbit.coffeeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
